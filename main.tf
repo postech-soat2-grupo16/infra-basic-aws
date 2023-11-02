dcb0de2c36af669f3af8047bf8467fbd1c13184f
Pedro Haramoto <pedroharamoto@gmail.com>
2023-11-02 18:42:40 -0300

    atualiza ports

--- main.tf
+++ main.tf
@@ -92,8 +92,8 @@ resource "aws_security_group" "security_group_load_balancer" {
   vpc_id      = aws_vpc.vpc_soat.id
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -117,8 +117,8 @@ resource "aws_security_group" "security_group_cluster_ecs" {
   vpc_id      = aws_vpc.vpc_soat.id
 
   ingress {
-    from_port       = 8080
-    to_port         = 8080
+    from_port       = 8000
+    to_port         = 8000
     protocol        = "tcp"
     security_groups = [aws_security_group.security_group_load_balancer.id]
   }
