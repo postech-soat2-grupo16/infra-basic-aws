30b38c5fb2897f96e530683baec054e83b766a56
Pedro Haramoto <pedroharamoto@gmail.com>
2023-11-03 13:04:04 -0300

    remove ECS

--- variables.tf
+++ variables.tf
@@ -12,20 +12,3 @@ variable "aws_az_b" {
   type    = string
   default = "us-east-1b"
 }
-
-variable "ecr_image" { 
-  description = "ECR Image"
-  type        = string
-  sensitive   = true
-}
-
-variable "execution_role_ecs" { 
-  description = "Execution role ECS"
-  type        = string
-  sensitive   = true
-}
-
-variable "desired_tasks" { 
-  type = number
-  default = 1
-}
